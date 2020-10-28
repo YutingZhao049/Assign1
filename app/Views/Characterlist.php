9c56210362a131586a921a3bf79d979f623d0b44
YutingZhao049 <29685@LAPTOP-K5EFA86I>
2020-10-29 00:39:56 +0800

    assign1

---
+++ app/Views/Characterlist.php
@@ -0,0 +1,13 @@
+<!doctype html>
+<html>
+	<head>
+		<title>DGPT4711 Assign1</title>
+	</head>
+	<body>
+		<h1>List of Twilight characters</h1>
+		{records}
+		<p><a href="/Twilightcharacters/showme/{id}">{name}</a></p>
+		{/records}
+		<p><a href="/home">Home</a></p>
+	</body>
+</html>
---
+++ app/Views/home.php
@@ -0,0 +1,11 @@
+<!doctype html>
+<html>
+	<head>
+		<title>DGPT4711 Assign1</title>
+	</head>
+	<body>
+		<h1>Twilight characters</h1>
+		<p><a href="/Twilightcharacters">Show the character</a>.</p>
+		<p><a href="/character">Show the character list</a></p>
+	</body>
+</html>
