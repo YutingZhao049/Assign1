ebe6b31a2d4c3e8d5fd653d78516c429f85b8a38
YutingZhao049 <29685@LAPTOP-K5EFA86I>
2020-10-29 00:39:12 +0800

    assign1

---
+++ app/Views/onecharacter.php
@@ -0,0 +1,18 @@
+<!doctype html>
+<html>
+	<head>
+		<title>DGPT4711 Assign1</title>
+	</head>
+	<body>
+		<h1>Twilight characters</h1>
+		<p><strong>ID:</strong> {id}</p>
+		<p><strong>Name:</strong> {name}</p>
+		<p><strong>Description:</strong> {description}</p>
+		<p><strong>gender:</strong> {gender}</p>
+                <p><strong>age:</strong> {age}</p>
+                <p><strong>address:</strong> {address}</p>
+                <p><strong>race:</strong> {race}</p>
+		<p><img src="/image/{image}"/></p>
+		<p><a href="/home">Home</a></p>
+	</body>
+</html>
